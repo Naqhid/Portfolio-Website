d61fbfae69901a2b947cbda66cee49339fd80f39
Mohammed Naqhid <mnaqhid@gmail.com>
2025-11-13 11:01:50 +0000

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio-Website/', // 👈 important for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
